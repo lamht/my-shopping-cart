0b25e43e77c8f96aabffb3d5f5791f98c2bc7f6a
lam <you@example.com>
2021-05-05 13:10:25 +0700

    Init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NoAccessComponent } from './shared/components/no-access/no-access.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    children: [
+      {
+        path: "",
+        loadChildren: () =>
+          import("./views/base/index/index.module").then((m) => m.IndexModule),
+      },
+      {
+        path: "products",
+        loadChildren: () =>
+          import("./views/pages/product/product.module").then(
+            (m) => m.ProductModule
+          ),
+      },
+      {
+        path: "users",
+        loadChildren: () =>
+          import("./views/pages/user/user.module").then((m) => m.UserModule),
+      },
+      {
+        path: "task-board",
+        loadChildren: () =>
+          import("./views/pages/task-board/task-board.module").then(
+            (m) => m.TaskBoardModule
+          ),
+      },
+    ],
+  },
+  { path: "no-access", component: NoAccessComponent },
+  { path: "**", component: PageNotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { APP_INITIALIZER, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TranslateService } from './shared/services/translate.service';
+import { SharedModule } from './shared/shared.module';
+import { IndexModule } from './views/base/index/index.module';
+import { ProductModule } from './views/pages/product/product.module';
+import { UserModule } from './views/pages/user/user.module';
+
+/* to load and set en.json as the default application language */
+export function setupTranslateFactory(service: TranslateService) {
+  return () => service.use("en");
+}
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IndexModule,
+    ProductModule,
+    UserModule,
+    SharedModule,
+  ],
+  providers: [
+    TranslateService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: setupTranslateFactory,
+      deps: [TranslateService],
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA],
+})
+export class AppModule { }
